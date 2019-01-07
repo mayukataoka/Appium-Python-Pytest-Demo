1c3d6968c2c57b2ec99d226ff6bd7b0d2879843c
MAYU KATAOKA <mayukataoka@MAYUs-MacBook-Pro.local>
2019-01-06 23:19:19 -0800

    use udid

--- test/test_android_notification.py
+++ test/test_android_notification.py
@@ -5,23 +5,32 @@ import textwrap
 from appium import webdriver
 from helpers import take_screenhot_and_logcat, ANDROID_APP_PATH, EXECUTOR
 
+PACKAGE = 'io.appium.android.apis'
+INCOMING_MESSAGE_ACTIVITY = '.app.IncomingMessage'
+
+
+def get_desired_capabilities():
+    desired_caps = {
+        'deviceName': os.getenv('UDID', 'Pixel_2_API_28'),
+        'udid': os.getenv('UDID', 'emulator-5556'),
+        'platformName': 'Android',
+        'app': ANDROID_APP_PATH,
+        'automationName': 'UIAutomator2',
+        'appActivity': INCOMING_MESSAGE_ACTIVITY
+    }
+
+    return desired_caps
+
+
 class TestAndroidBasicInteractions():
-    PACKAGE = 'io.appium.android.apis'
-    INCOMING_MESSAGE_ACTIVITY = '.app.IncomingMessage'
+
 
     @pytest.fixture(scope='function')
     def driver(self, request, device_logger):
         calling_request = request._pyfuncitem.name
         driver = webdriver.Remote(
             command_executor=EXECUTOR,
-            desired_capabilities={
-                'app': ANDROID_APP_PATH,
-                'platformName': 'Android',
-                'automationName': 'UIAutomator2',
-                'platformVersion': os.getenv('ANDROID_PLATFORM_VERSION') or '9',
-                'deviceName': os.getenv('ANDROID_DEVICE_VERSION') or 'emulator-5554',
-                'appActivity': self.INCOMING_MESSAGE_ACTIVITY
-            }
+            desired_capabilities=get_desired_capabilities()
         )
 
         yield driver
@@ -33,6 +42,8 @@ class TestAndroidBasicInteractions():
 
         notification_trigger_button = driver.find_element_by_id('notify_app')
         notification_trigger_button.click()
+        driver.implicitly_wait(100)
+
         driver.open_notifications()
         notification_from_joe = driver.find_element_by_id('android:id/title')
 
